642a123e8630c568b0c9b5545b486667f9ae5d01
Zoya S <zoya.shakil@motsd.org>
2026-02-28 11:07:07 -0500

    removed right intake + added extra motor for feeder

--- src/main/java/org/mort11/commands/autons/pathplanner/BasicCommands.java
+++ src/main/java/org/mort11/commands/autons/pathplanner/BasicCommands.java
@@ -6,7 +6,6 @@ import org.mort11.commands.actions.endeffector.pid.SetEvanHood;
 import org.mort11.commands.actions.endeffector.pid.SetShooter;
 import org.mort11.commands.actions.endeffector.pid.SetSuperShooter;
 import org.mort11.commands.actions.endeffector.pid.SetTurret;
-import org.mort11.commands.actions.endeffector.pid.setIntakeRight;
 import org.mort11.commands.autons.timed.Taxi;
 import org.mort11.configs.constants.PhysicalConstants;
 import org.mort11.subsystems.CommandSwerveDrivetrain;
