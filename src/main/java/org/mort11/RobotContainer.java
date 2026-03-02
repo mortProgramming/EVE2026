c003f65f57acedcf098effba799713207ec9888b
Sammy <sammy.bikki@gmail.com>
2026-03-02 16:24:54 -0500

    Set up supershooter

--- src/main/java/org/mort11/RobotContainer.java
+++ src/main/java/org/mort11/RobotContainer.java
@@ -207,20 +207,16 @@ public class RobotContainer {
             manualController.b().onTrue(setIntakeLeft.up());
             //endeffectorController.pov(180).onTrue(setIntakeLeft.intake());
             //endeffectorController.pov(0).onTrue(setIntakeLeft.up());
-
             //manualController.x().onTrue(setIntakeRight.intake());
             //manualController.y().onTrue(setIntakeRight.up());
-         //endeffectorController.a().onTrue(setIntakeRight.intake());
+            //endeffectorController.a().onTrue(setIntakeRight.intake());
             //endeffectorController.y().onTrue(setIntakeRight.up());
-            
-
-            
-
+        
             //Feeder
             manualController.pov(0).whileTrue(new moveFeeder(0.5));
             manualController.pov(180).whileTrue(new moveFeeder(-1)); //moves the correct way
             endeffectorController.rightTrigger(TRIGGER_THRESHOLD).whileTrue(new moveFeeder(-1));
-
+        
             //Turret
             manualController.pov(90).whileTrue(new MoveTurret(-MANUAL_SPEED));
             manualController.pov(270).whileTrue(new MoveTurret(MANUAL_SPEED));
@@ -229,22 +225,22 @@ public class RobotContainer {
                 .whileTrue(new MoveTurret(endeffectorController.getRightX() * MANUAL_SPEED));
             new Trigger(() -> endeffectorController.getRightX() < -DEAD_BAND)
                 .whileTrue(new MoveTurret(endeffectorController.getRightX() * MANUAL_SPEED));
-
+        
             //hood
             manualController.back().whileTrue(new MoveEvanHood(1));
             manualController.start().whileTrue(new MoveEvanHood(-1));
-
+        
             //Shooter
             manualController.y().whileTrue(new PercentShoot(0.25));
-
+        
             endeffectorController.leftTrigger(TRIGGER_THRESHOLD).whileTrue(new SetShooter(2500));
-
+        
             
             // endeffectorController.x().whileTrue(new SetShooter(3000));
             // LookUpTable.getNeededHoodAngle(-1);
             // LookUpTable.getNeededHoodAngle(3);
             // LookUpTable.getNeededHoodAngle(300);
-
+        
             // endeffectorController.x().whileTrue(
             //     new SetHoodShooter(
             //         () -> LookUpTable.getNeededShooterRPM(odometry.getDistanceToHub()),
@@ -254,10 +250,10 @@ public class RobotContainer {
             
             endeffectorController.x().whileTrue(new SetSuperShooter(
                 () -> LookUpTable.getNeededShooterRPM(odometry.getDistanceToHub()), 
-                () -> 0, 
+                () -> odometry.getAngleForTurretDeg(),
                 () -> LookUpTable.getNeededHoodAngle(odometry.getDistanceToHub())
             ));
-
+        
             //Climber
             manualController.leftBumper().whileTrue(new Climb(0.5));
             manualController.rightBumper().whileTrue(new Climb(-0.5));
