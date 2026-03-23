7847c9722d13601ce2cf25664bde32fa156b82e5
Zoya S <zoya.shakil@motsd.org>
2026-03-23 16:24:38 -0400

    lock on

--- src/main/java/org/mort11/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/org/mort11/subsystems/CommandSwerveDrivetrain.java
@@ -181,7 +181,7 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
 		aprilTagYController = new PIDController(1.1, 0, 0);
 		aprilTagYController.setTolerance(0.05);
 
-		aprilTagOmegaController = new PIDController(0.04, 0, 0);
+		aprilTagOmegaController = new PIDController(1, 0, 0);
 		aprilTagOmegaController.setTolerance(0.05);
     }
 
