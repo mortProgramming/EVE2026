693837dea1a8b2a97faf65e664d67f754d8d58e6
Adam Soliman <adam.soliman@motsd.org>
2026-04-11 15:11:40 -0400

    Added Phase Timer to Smart Dashboard

--- src/main/java/org/mort11/Robot.java
+++ src/main/java/org/mort11/Robot.java
@@ -5,6 +5,9 @@
 package org.mort11;
 
 import org.mort11.subsystems.Vision;
+
+import java.sql.Driver;
+
 import org.mort11.subsystems.LimelightRewindManager;
 
 import com.ctre.phoenix6.HootAutoReplay;
@@ -12,7 +15,8 @@ import com.ctre.phoenix6.HootAutoReplay;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
 
@@ -71,7 +75,8 @@ public class Robot extends TimedRobot {
 
     @Override
     public void autonomousPeriodic() {
-        
+        SmartDashboard.putNumber("Remaining Phase Time",DriverStation.getMatchTime());
+        SmartDashboard.putString("Current Phase", "Autonomous");
     }
 
     @Override
@@ -90,7 +95,17 @@ public class Robot extends TimedRobot {
     }
     
     @Override
-    public void teleopPeriodic() {}
+    public void teleopPeriodic() {
+        final int[] phaseStartTimes = {140, 130, 105, 80, 55, 30, 0}; // in seconds
+        final String[] phaseNames = {"Transition Shift", "Phase 1", "Phase 2", "Phase 3", "Phase 4", "Endgame", "End of Match"};
+        for(int i=0; i<phaseStartTimes.length-1; i++){
+            if(DriverStation.getMatchTime() <= phaseStartTimes[i] && DriverStation.getMatchTime() > phaseStartTimes[i+1]){
+                SmartDashboard.putNumber("Remaining Phase Time", DriverStation.getMatchTime()-phaseStartTimes[i+1]);
+                SmartDashboard.putString("Current Phase", phaseNames[i]);
+                break;
+            }
+        }
+    }
 
     @Override
     public void teleopExit() {}
