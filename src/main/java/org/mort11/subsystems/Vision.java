efdd7012b503874f7788a7ad41596730d0e5e585
RBEAB23 <abrattstrom@wpi.edu>
2026-04-11 12:55:11 -0400

    Added: limelight-back pose

--- src/main/java/org/mort11/subsystems/Vision.java
+++ src/main/java/org/mort11/subsystems/Vision.java
@@ -86,19 +86,19 @@ public class Vision extends SubsystemBase {
             0.0  * 0.0254,   // side — (should be at 0 inches)
             25.0 * 0.0254,   // up — (about 25 inches off the floor)
             0.0,             // roll degrees: (should be 0)
-            -16.0,               // pitch degrees: (rotating up) ==> negative rotation around Y-axis). Measured about -16 degrees from onboard IMU (http://limelight-three.local:5801/)
+            -27.30,               // pitch degrees: (rotating up) ==> negative rotation around Y-axis). Measured about -16 degrees from onboard IMU (http://limelight-three.local:5801/)
             0.0              // yaw degrees: (should be 0)
         );
 
-        // LimelightHelpers.setCameraPose_RobotSpace(
-        // "limelight-back",
-        // -3.0 * 0.0254,
-        // 0.0  * 0.0254,   // side — (should be at 0 inches)
-        // 25.0 * 0.0254,   // up — (about 25 inches off the floor)
-        // 0.0,             // roll degrees: (should be 0)
-        // -16.0,             // pitch degrees: (rotating up) ==> negative rotation around Y-axis). Measured about -16 degrees from onboard IMU (http://limelight-three.local:5801/)
-        // 180
-        // );
+        LimelightHelpers.setCameraPose_RobotSpace(
+            "limelight-back",
+            -15.0 * 0.0254,
+            0.0  * 0.0254,   // side — (should be at 0 inches)
+            18.0 * 0.0254,   // up — (about 25 inches off the floor)
+            0.0,             // roll degrees: (should be 0)
+            -24.90,             // pitch degrees: (rotating up) ==> negative rotation around Y-axis). Measured about -16 degrees from onboard IMU (http://limelight-three.local:5801/)
+            180
+        );
     }
 
     @Override
